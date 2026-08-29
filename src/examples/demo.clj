3ab827d8ae60f5eef777d2ba491b878708672ba9
habruening <hbrng.computer@mailbox.org>
2026-08-29 20:29:52 +0200

    demonstration of session data

--- src/examples/demo.clj
+++ src/examples/demo.clj
@@ -12,33 +12,36 @@
 (defn name-line [i]
   [:tr {:id i}
    [:td (@data i)]
-   [:td [:button {:data-on:click (w*/on-server '(demo/upper (on-client session) ~i))} "to upper case"]]
-   [:td [:button {:data-on:click (w*/on-server '(demo/lower (on-client session) ~i))} "to lower case"]]
-   [:td [:button {:data-on:click (w*/on-server '(demo/remove (on-client session) ~i))} "x"]]])
+   [:td [:button {:data-on:click (w*/on-server '(demo/upper ~i))} "to upper case"]]
+   [:td [:button {:data-on:click (w*/on-server '(demo/lower ~i))} "to lower case"]]
+   [:td [:button {:data-on:click (w*/on-server '(demo/remove ~i))} "x"]]
+   [:td [:button {:data-on:click (w*/on-server '(demo/invite (on-client session) ~(@data i)))} "invite"]]])
 
 (defn people []
   [:table#contents (map name-line (range (count @data)))])
 
-(defn upper [session i]
+(defn upper [i]
   (swap! data update i clojure.string/upper-case)
   (w*/patch-for-everybody (name-line i)))
 
-(defn lower [session i]
+(defn lower [i]
   (swap! data update i clojure.string/lower-case)
   (w*/patch-for-everybody (name-line i)))
 
 (defn remover [i v]
   (into (subvec v 0 i) (subvec v (inc i) (count v))))
 
-(defn remove [session i]
+(defn remove [i]
   (swap! data (partial remover i))1
   (w*/patch-for-everybody (people)))
 
-(defn add [session name]
+(defn add [name]
   (swap! data #(conj % name))
   (w*/patch-for-everybody (people)))
 
-(w*/patch-for-everybody (people))
+(defn invite [session name]
+  (w*/swap!-session-data session #(conj % name))
+  (w*/patch session [:span#audience (clojure.string/join ", " (w*/session-data session))]))
 
 (defn main-html []
   (let [session (str (random-uuid))]
@@ -49,9 +52,8 @@
       [:div {:data-init (str "@get('/connect?session=' + session)")}]
       (people)
       [:input {:data-bind :name}]
-      [:button {:data-on:click (w*/on-server '(demo/add (on-client session) (on-client $name)))} "add"]]]))
-
-(h/html (main-html))
+      [:button {:data-on:click (w*/on-server '(demo/add (on-client $name)))} "add"]
+      [:div "Audience: " [:span#audience ""]]]]))
 
 (defroutes app
   (GET "/" [] (do (println "new session")
